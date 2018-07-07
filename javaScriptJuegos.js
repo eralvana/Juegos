625bcd7669707574b89d86b5e6d75eaa08b1051e
Shino <40175125+eralvana@users.noreply.github.com>
2018-07-07 18:19:24 -0500

    Add files via upload

--- javaScriptJuegos.js
+++ javaScriptJuegos.js
@@ -11,8 +11,27 @@ function juego01() {
 	var tablaPosicion = (canvas.width - tablaAncho)/2;
 	var pucharDerecho = false;
 	var pucharIzquierdo = false;
-	document.addEventListener("keydown", oprimir, false);
+    var renglonesBloques = 5;
+    var columnasBloques = 8;
+    var bloqueAncho = 55;
+    var bloqueAlto = 15;
+    var bloqueEspacio = 3;
+    var bloqueMargenSuperior = 10;
+    var bloqueMargenInferior = 10;
+    var techoBloques = 30;
+    var paredBloques = 10;
+    var puntos = 0;
+    var oportunidades = 3;
+    var bloques = [];
+    for(c=0; c<columnasBloques; c++) {
+        bloques[c] = [];
+        for(r=0; r<renglonesBloques; r++) {
+            bloques[c][r] = { x: 0, y: 0, estado: 1};
+        }
+    }
+    document.addEventListener("keydown", oprimir, false);
 	document.addEventListener("keyup", nooprimir, false);
+    document.addEventListener("mousemove", moverRaton, false);
 	function oprimir(e) {
 		if(e.keyCode == 39) {
 			pucharDerecho = true;
@@ -29,6 +48,12 @@ function juego01() {
     		pucharIzquierdo = false;
     	}
     }
+    function moverRaton(e) {
+        var ratonPosicion = e.clientX - canvas.offsetLeft;
+        if(ratonPosicion > 0 && ratonPosicion < canvas.width) {
+            tablaPosicion = ratonPosicion - tablaAncho/2;
+        }
+    }
     function pelota() {
 		contexto.beginPath();
     	contexto.arc(x, y, radio, 0, Math.PI*2);
@@ -43,10 +68,59 @@ function juego01() {
     	contexto.fill();
     	contexto.closePath();
     }
+    function bloque() {
+        for(c=0; c<columnasBloques; c++) {
+            for(r=0; r<renglonesBloques; r++) {
+                if (bloques[c][r].estado == 1) {
+                var bloquePosiciónX = (c*(bloqueAncho+bloqueEspacio))+paredBloques;
+                var bloquePosiciónY = (r*(bloqueAlto+bloqueEspacio))+techoBloques;
+                bloques [c][r].x = bloquePosiciónX;
+                bloques[c][r].y = bloquePosiciónY;
+                contexto.beginPath();
+                contexto.rect(bloquePosiciónX, bloquePosiciónY, bloqueAncho, bloqueAlto);
+                contexto.fillStyle = "#00FF00";
+                contexto.fill();
+                contexto.closePath();
+                }
+            }
+        }
+    }
+    function detectar() {
+        for(c=0; c<columnasBloques; c++) {
+        for(r=0; r<renglonesBloques; r++) {
+            var b = bloques[c][r];
+            if(b.estado == 1) {
+                if(x > b.x && x < b.x+bloqueAncho && y > b.y && y < b.y+bloqueAlto) {
+                    dy = -dy;
+                    b.estado = 0;
+                    puntos++;
+                    if(puntos == renglonesBloques*columnasBloques) {
+                        alert("¡Ganaste!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+        }
+    }
+    function puntuacion() {
+        contexto.font = "bold 16px verdana";
+        contexto.fillStyle = "#000000";
+        contexto.fillText("Puntuación: "+puntos, canvas.width-200, 20);
+    }
+    function oportunidad() {
+        contexto.font = "bold 16px verdana";
+        contexto.fillStyle = "#000000";
+        contexto.fillText("Oportunidades: "+oportunidades, 10, 20);
+    }
     function pintar() {
     	contexto.clearRect(0, 0, canvas.width, canvas.height);
+        bloque();
     	pelota();
     	tabla();
+        puntuacion();
+        oportunidad();
+        detectar();
     	if(x + dx > canvas.width-radio || x + dx < radio) {
     		dx = -dx;
     	}
@@ -58,9 +132,19 @@ function juego01() {
     			dy = -dy;
     		}
     		else {
-    			alert("¡Perdiste!");
-    			document.location.reload();
-    		}
+                oportunidades--;
+                if(!oportunidades) {
+                    alert("¡Perdiste! ¿Solamente pudiste con " + puntos + " bloques?");
+                    document.location.reload();
+                }
+                else {
+                    x = canvas.width/2;
+                    y = canvas.height-30;
+                    dx = 2;
+                    dy = -2;
+                    tablaPosicion = (canvas.width-tablaAncho)/2;
+                }
+            }
     	}
     	if(pucharDerecho && tablaPosicion < canvas.width - tablaAncho) {
     		tablaPosicion += 8;
@@ -70,6 +154,7 @@ function juego01() {
     	}
     	x += dx;
     	y += dy;
+        requestAnimationFrame(pintar);
     }
-    setInterval(pintar, 10);
+    pintar();
 }
