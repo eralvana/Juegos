ce73cae9d6d34c4c9fd98df77bf4ab460c429b2d
Shino <40175125+eralvana@users.noreply.github.com>
2018-07-27 13:29:17 -0500

    Add files via upload

--- Prueba/Prueba.js
+++ Prueba/Prueba.js
@@ -1,3 +1,5 @@
+var col;
+var ren;
 function cambiaColor(id) {
   var colorActual = document.getElementById(id).getAttribute("class");
   var color = ["c0", "c1", "c2", "c3","c4"]
@@ -10,3 +12,23 @@ function cambiaColor(id) {
 function comprobar(){
 
 }
+
+
+
+function genera_tabla() {
+    ren = document.getElementById("renglones").value;
+    col = document.getElementById("columnas").value;
+    /*var contenedor = document.getElementsByClassName("wrapper");
+    contenedor[0].setAttribute("grid-template-columns", '75px');
+    contenedor[0].setAttribute("grid-template-rows", "75px");*/
+    for(i=0;i< ren ;i++){
+        for(j=0;j<col;j++){
+            var midiv = document.createElement("div");
+            midiv.setAttribute("id","c"+i+j);
+            midiv.setAttribute("class","c0");
+            midiv.addEventListener("click", function(){cambiaColor(this.id)});
+            midiv.innerHTML = "c"+i+j;
+            document.getElementById("tabla").appendChild(midiv);
+            }
+        }
+    }
