a2b915c5d591dc36ac5f41fa85fb6fb71503b714
Shino <40175125+eralvana@users.noreply.github.com>
2018-07-30 17:53:48 -0500

    Add files via upload

--- Prueba/Prueba.js
+++ Prueba/Prueba.js
@@ -9,34 +9,32 @@ function genera_tabla() {
   otrodiv.setAttribute("id", "btn")
   document.getElementById("botoncito").appendChild(otrodiv);
   var boton = document.createElement("input");
+  boton.setAttribute("id","revisar")
   boton.setAttribute("type","button");
   boton.setAttribute("value","Validar");
-  boton.setAttribute("click",comprobar());
+  boton.addEventListener("click",comprobar);
   document.getElementById("btn").appendChild(boton);
+  var dimension = document.getElementById("aquivalochido");
+  dimension.setAttribute("style", "width:" + col*80 + "px");
   var contenedor = document.createElement("div");
   contenedor.setAttribute("class","wrapper");
   contenedor.setAttribute("id","tabla");
+  contenedor.setAttribute("style","grid-template-columns: repeat(" + col + ", 75px)");
   document.getElementById("aquivalochido").appendChild(contenedor);
-  
-
-  
-
-
   for(i=0;i< ren ;i++){
     for(j=0;j<col;j++){
       var midiv = document.createElement("div");
       midiv.setAttribute("id","c"+i+j);
       midiv.setAttribute("class","c0");
+      midiv.setAttribute("style", "grid-template-columns: repeat(" + col + ", 75px)");
+      midiv.setAttribute("style","height: 75px");
+      //midiv.setAttribute("style", "grid-template-rows: repeat("+ ren + ", 75px)");
       midiv.addEventListener("click", function(){cambiaColor(this.id)});
       midiv.innerHTML = "Casilla "+i+j;
       document.getElementById("tabla").appendChild(midiv);
       }
     }
 }
-
-
-
-
 function cambiaColor(id) {
   var colorActual = document.getElementById(id).getAttribute("class");
   var color = ["c0", "c1", "c2", "c3","c4"]
@@ -47,5 +45,13 @@ function cambiaColor(id) {
   document.getElementById(id).setAttribute("class", color[i]);
 }
 function comprobar(){
- 
+  alert("Prueba de que jala dos trenzas el botón.")
+  /*for(i=0;i< ren ;i++){
+    for(j=0;j<col;j++){
+      var cas = document.getElementById("c"+i+j).getAttribute("class");
+      while(cas !== "c0"){};
+      alert("La Casilla "+  i + "," + j + "  está en blanco")
+
+      }
+    }*/
 }
