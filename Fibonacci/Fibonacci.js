c3f884ce4ecd29ff1b7aa24c5e82b0d7cf2df484
Shino <40175125+eralvana@users.noreply.github.com>
2018-08-01 13:59:21 -0500

    Add files via upload

--- Fibonacci/Fibonacci.js
+++ Fibonacci/Fibonacci.js
@@ -1,5 +1,16 @@
 function encontrar() {
-  var num = document.getElementById("numberF").value;
+  var num = document.getElementById("numberFe").value;
   var term = (Math.pow(((1+Math.sqrt(5))/2),num) - Math.pow((1-Math.sqrt(5))/2,num))/Math.sqrt(5);
   alert(term)
+}
+function apata(){
+	var num = document.getElementById("numberFp").value;
+    var a0 = 0;
+    var a1 = 1;
+    for(i=0;i < num; i++){
+    	var base = a0;
+    	a0 = a1;
+    	a1 = base + a1;
+    }
+    alert(a0)
 }
